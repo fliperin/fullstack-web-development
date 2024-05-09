57a3aca62bf7f7fc8a54a0698c6ca51da493bff4
Filipe Paza <filipe.cruz8@etec.sp.gov.br>
2024-05-09 16:31:42 +0000

    Configure HMR on Gitpod & localhost

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			server: {
+				hmr: {
+					clientPort: process.env.HMR_HOST ? 443 : 5173,
+					host: process.env.HRM_HOST ? process.env.HRM_HOST.substring("https://".length) : "localhost"
+				}
+			}
+		}
 	}
 };
 
